ac4d5b07e1e6e924b5fa4345c1352e09b81fd960
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-21 08:38:10 +0300

    Remove initial content and Adding StyleGuide.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Raleway, Outfit } from 'next/font/google';
+
+export const raleway = Raleway({
+  weight: ['500', '600'],
+  subsets: ['latin'],
+});
+
+export const outfit = Outfit({
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+});
