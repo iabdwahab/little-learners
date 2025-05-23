d9ed43368ddf2ba28525077373acdf2afdcf1fd3
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-23 19:06:03 +0300

    SectionHeading: Finishing.

---
+++ app/components/SectionHeading.tsx
@@ -0,0 +1,12 @@
+function SectionHeading() {
+  return (
+    <div className="grid justify-center gap-2.5 text-center lg:gap-3.5">
+      <p className="text-grey-20 mx-auto w-fit rounded-md border-2 bg-white px-3.5 py-2 font-medium">Children Deserve Bright Future</p>
+      <h2 className="font-raleway text-4xl font-bold lg:text-5xl">Our Benefits</h2>
+      <p className="max-w-[880px] font-medium">
+        With a dedicated team of experienced educators, state-of-the-art facilities, and a comprehensive curriculum, we aim to lay a strong foundation for your child's future.
+      </p>
+    </div>
+  );
+}
+export default SectionHeading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HomeLanding from "./components/home_landing/Index";
+import SectionHeading from "./components/SectionHeading";
 
 export default function Home() {
   return (
-    <main className="[&>*]:my-8">
+    <main className="[&>*]:my-20 lg:[&>*]:my-24">
       <HomeLanding />
+      <SectionHeading />
     </main>
   );
 }
