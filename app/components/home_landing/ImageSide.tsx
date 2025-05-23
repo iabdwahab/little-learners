04cd1ae510042912012dd1055efa98ce04eeb4e0
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-23 17:42:03 +0300

    HomeLanding: Modify Image Height.

--- app/components/home_landing/ImageSide.tsx
+++ app/components/home_landing/ImageSide.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function ImageSide() {
   return (
     <div className="bg-orange-80 p-2">
-      <div className="flex h-[410px] items-center justify-center border-2 lg:h-auto">
+      <div className="flex h-96 max-h-[500px] items-center justify-center border-2 lg:h-[calc(100vh-80px)]">
         <Image className="h-11/12 w-11/12" src="/shapes/circled-circles.svg" alt="Illustration" width={600} height={400} />
       </div>
     </div>
--- app/components/home_landing/StatsContainer.tsx
+++ app/components/home_landing/StatsContainer.tsx
@@ -2,7 +2,7 @@ import { landingStatsElemnts } from "@/app/data/homeLandingStats";
 
 function StatsContainer() {
   return (
-    <div className="bg-orange-95 flex flex-col items-center divide-y-2 rounded-[10px] border-2 p-10 text-center md:flex-row md:gap-4 md:divide-y-0 lg:col-span-2 xl:text-left [&>*]:flex-1">
+    <div className="bg-orange-95 flex flex-col items-center divide-y-2 rounded-[10px] border-2 p-10 text-center md:flex-row md:gap-4 md:divide-y-0 lg:col-span-2 lg:py-8 xl:text-left [&>*]:flex-1">
       {landingStatsElemnts}
     </div>
   );
