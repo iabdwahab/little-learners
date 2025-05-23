6bc269ee0e050d5affa21cab5857909b2d134b67
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-23 12:22:16 +0300

    HeaderNavbar: Hide navbar when pathname change.

--- app/components/header/Index.tsx
+++ app/components/header/Index.tsx
@@ -4,10 +4,15 @@ import NavbarMobile from "./mobile/NavbarMobile";
 import NavbarDesktop from "./dekstop/NavbarDesktop";
 import NavbarToggleButton from "./NavbarToggleButton";
 import Logo from "./Logo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { PathnameContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [PathnameContext]);
 
   return (
     <header className="text-grey-15 relative mt-3.5 flex items-stretch justify-between rounded-lg border-2 bg-white">
