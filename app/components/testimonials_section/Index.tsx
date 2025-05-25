80a8d24c4eb968a494fbd334f139889b4b90fec3
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-25 12:13:46 +0300

    TestimonialsSection: Implementing TestimonialCard.

---
+++ app/components/testimonials_section/Index.tsx
@@ -0,0 +1,17 @@
+import SectionHeading from "../section_heading/Index";
+import TestimonialCard from "./TestimonialCard";
+
+function TestimonialsSection() {
+  return (
+    <section>
+      <SectionHeading>
+        <SectionHeading.Slogan>Their Happy Words 🤗</SectionHeading.Slogan>
+        <SectionHeading.Title>Our Testimonials</SectionHeading.Title>
+        <SectionHeading.Description>
+          Our testimonials are heartfelt reflections of the nurturing environment we provide, where children flourish both academically and emotionally.
+        </SectionHeading.Description>
+      </SectionHeading>
+    </section>
+  );
+}
+export default TestimonialsSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import BenefitsSection from "./components/BenefitsSection";
 import HomeLanding from "./components/home_landing/Index";
+import TestimonialsSection from "./components/testimonials_section/Index";
 
 export default function Home() {
   return (
     <main className="[&>*]:mb-20 lg:[&>*]:mb-24">
       <HomeLanding className="mt-6" />
       <BenefitsSection />
+      <TestimonialsSection />
     </main>
   );
 }
