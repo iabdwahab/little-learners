59b6c846b3a5d42ec4a12d32d999085efb6fdee8
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-28 21:26:54 +0300

    NavigatingSection: Fixing LearnMoreLink Shrinking.

--- app/components/navigating_section/NavigatingCard.tsx
+++ app/components/navigating_section/NavigatingCard.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 function NavigatingCard({ title, description, url }: NavigatingCardInterface) {
   return (
-    <div className="shadow-main rounded-[10px] border-2 bg-white p-10 text-center lg:p-[60px]">
+    <div className="shadow-main flex flex-col rounded-[10px] border-2 bg-white p-10 text-center lg:p-[60px]">
       <h4 className="font-raleway text-grey-10 mb-5 text-[28px] font-bold lg:text-4xl">{title}</h4>
       <p className="text-grey-30 mb-[40px] font-medium">{description}</p>
-      <Link href={url} className="bg-orange-90 shadow-main flex items-center justify-center gap-2.5 rounded-md border-[2px] p-4 font-medium">
+      <Link href={url} className="bg-orange-90 shadow-main mt-auto flex items-center justify-center gap-2.5 rounded-md border-[2px] p-4 font-medium">
         <span className="text-grey-20 lg:text-lg">Learn More</span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-grey-20 size-5 lg:size-6">
           <path
