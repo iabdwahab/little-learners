44bad73a8746596e07edff2a443e119844149158
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-30 18:58:52 +0300

    Handle notFounded Paths.

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+function NotFound() {
+  return (
+    <main className="py-40">
+      <h1 className="text-grey-10 text-center text-4xl font-medium">Sorry, the page you are looking for doesn't exist.</h1>
+    </main>
+  );
+}
+export default NotFound;
