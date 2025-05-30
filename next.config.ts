0a40413078713e3f8244195bdafc3abde30bf87f
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-30 19:06:46 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/little-learners",
+  assetPrefix: "/little-learners/",
 };
 
 export default nextConfig;
